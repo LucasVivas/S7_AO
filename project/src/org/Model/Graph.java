db142ce45e2585dce65e1af424524bb7d658066c
Lucas Vivas <lucas.vivas33@gmail.com>
2017-11-19 20:00:49 +0100

    add lib and test

--- project/src/org/Model/Graph.java
+++ project/src/org/Model/Graph.java
@@ -1,9 +1,34 @@
 package org.Model;
-/**
- * Created by lulu on 19/11/17.
- */
+
+import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.DirectedAcyclicGraph;
 
+/**
+ * Created by lulu (project's boss) on 19/11/17.
+ * Graph class represents the labyrinth graph.
+ */
 public class Graph {
-    private DirectedAcyclicGraph graph;
+    private static Graph graph = null;
+    private DirectedAcyclicGraph<String,DefaultEdge> DAGraph = null;
+
+    private Graph() {
+        super();
+
+    }
+
+    private void buildLabyrinthGraph(String v){
+
+    }
+    /**
+     * Return the unique instance of Graph.
+     * @return null if the instance is not already created else
+     * return the instance.
+     */
+    public static Graph getInstance(){
+        if (Graph.graph == null){
+            graph = new Graph();
+            return graph;
+        }
+        return null;
+    }
 }
