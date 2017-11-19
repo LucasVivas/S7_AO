f380d5c2c6fdfe3c6db4904cf5cc52bf56ab221a
Lucas Vivas <lucas.vivas33@gmail.com>
2017-11-19 20:01:20 +0100

    add GraphTest

---
+++ project/src/org/Model/GraphTest.java
@@ -0,0 +1,16 @@
+package org.Model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by lulu on 19/11/17.
+ */
+public class GraphTest {
+    @Test
+    public void getInstance() throws Exception {
+
+    }
+
+}
