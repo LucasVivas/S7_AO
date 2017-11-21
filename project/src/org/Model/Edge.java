63de04530acfc4cb51e47b75ecd92bfb4d5c40cb
Lucas Vivas <lucas.vivas33@gmail.com>
2017-11-21 17:36:20 +0100

    add vertex.compareTo() & class edge

---
+++ project/src/org/Model/Edge.java
@@ -0,0 +1,9 @@
+package org.Model;
+
+/**
+ * Created by lulu on 21/11/17.
+ */
+public class Edge {
+    private Vertex v;
+    private Vertex u;
+}
--- project/src/org/Model/Vertex.java
+++ project/src/org/Model/Vertex.java
@@ -3,12 +3,20 @@ package org.Model;
 /**
  * Created by lulu on 21/11/17.
  */
-public class Vertex {
+public class Vertex implements Comparable<Vertex>{
     private int x;
     private int y;
 
     public Vertex(int x, int y){
         this.x = x;
+        this.y = y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
         this.y = y;
     }
 
@@ -26,4 +34,20 @@ public class Vertex {
         return (dx == 0 && dy == 1) || (dx == 1 && dy == 0);
     }
 
+    @Override
+    public int compareTo(Vertex v) {
+        int dx = this.getX() - v.getX();
+        int dy = this.getY() - v.getY();
+        if (dx < 0)
+            return -1;
+        else if (dx > 0)
+            return 1;
+        else {
+            if (dy < 0)
+                return -1;
+            else if (dy > 0)
+                return 1;
+        }
+        return 0;
+    }
 }
