c701831f4c2c43d887ac8390d91a8cd4af8c876f
Lucas Vivas <lucas.vivas33@gmail.com>
2017-11-23 19:33:05 +0100

    equals vertex

--- project/src/org/Model/VertexTest.java
+++ project/src/org/Model/VertexTest.java
@@ -8,16 +8,33 @@ public class VertexTest {
     public void getX() throws Exception {
         Vertex vertex1 = new Vertex(10,21, 6);
         Vertex vertex2 = new Vertex(0,0,0);
+        Vertex vertex3 = null;
+
         assertEquals(10,vertex1.getX());
         assertEquals(0,vertex2.getX());
+
+        try {
+            vertex3.getX();
+            fail("NullPointerException need to be throw");
+        }catch (NullPointerException npe){
+            //
+        }
     }
 
     @Test
     public void getY() throws Exception {
         Vertex vertex1 = new Vertex(10,21, 6);
         Vertex vertex2 = new Vertex(0,0,0);
+        Vertex vertex3 = null;
+
         assertEquals(21,vertex1.getY());
         assertEquals(0,vertex2.getY());
+        try {
+            vertex3.getY();
+            fail("NullPointerException need to be throw");
+        }catch (NullPointerException npe){
+            //
+        }
     }
 
     @Test
