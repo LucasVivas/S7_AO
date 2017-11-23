4517ff81cd094642386bfccea7cb62f909102937
Lucas Vivas <lucas.vivas33@gmail.com>
2017-11-23 15:09:30 +0100

    je sais pas ou j'en suis

--- project/src/org/Model/Graph.java
+++ project/src/org/Model/Graph.java
@@ -2,60 +2,56 @@ package org.Model;
 
 import org.jgrapht.graph.SimpleGraph;
 
+import java.util.Random;
+import java.util.Vector;
+
 /**
- * Created by lulu (project's boss) on 19/11/17.
- * Graph class represents the labyrinth graph.
+*Created by lulu (project's boss) on 19/11/17.
+ * Graphgh class represents the labyrinth graph.
  */
 public class Graph {
-    public static final int TOP_BORDER = 0; //to move
-    public static final int BOTTOM_BORDER = 4; //to move
-    public static final int LEFT_BORDER = 0; //to move
-    public static final int RIGHT_BORDER = 4; //to move
+    public static final int TOP_BORDER = 0;
+    public static final int BOTTOM_BORDER = 4;
+    public static final int LEFT_BORDER = 0;
+    public static final int RIGHT_BORDER = 4;
 
+    public Random random = new Random();
     private SimpleGraph<Vertex,Edge> graph = null;
 
     private Graph(Vertex origin) {
         graph = new SimpleGraph<>(Edge.class);
-        buildLabyrinthGraph(origin);
+        buildRandomPath(origin);
     }
 
-    private void buildLabyrinthGraph(Vertex v){
-        CardinalsPoints c = new CardinalsPoints(4);
-        Vertex u;
-        for (int i = 0; i < c.getSize(); i++) {
-            switch (c.getArrayInd(i)){
-                case NORTH:
-                    u = new Vertex(v.getX(),v.getY()+1);
-                    if (!graph.containsVertex(u) && u.getY() > TOP_BORDER) {
-                        graph.addVertex(u);
-                        graph.addEdge(v, u);
-                        buildLabyrinthGraph(u);
-                    }
-                    break;
-                case EAST:
-                    u = new Vertex(v.getX()+1,v.getY());
-                    if (!graph.containsVertex(u) && u.getX() > RIGHT_BORDER) {
-                        graph.addVertex(u);
-                        graph.addEdge(v, u);
-                        buildLabyrinthGraph(u);
-                    }
-                    break;
-                case SOUTH:
-                    u = new Vertex(v.getX(),v.getY()-1);
-                    if (!graph.containsVertex(u) && u.getY() > BOTTOM_BORDER) {
-                        graph.addVertex(u);
-                        graph.addEdge(v,u);
-                        buildLabyrinthGraph(u);
-                    }
-                    break;
-                case WEST:
-                    u = new Vertex(v.getX()-1,v.getY());
-                    if (!graph.containsVertex(u) && u.getX() > LEFT_BORDER) {
-                        graph.addVertex(u);
-                        graph.addEdge(v,u);
-                        buildLabyrinthGraph(u);
-                    }
-                    break;
+    public void buildRandomPath ( Vertex vertex ) {
+        // une liste aleatoire des 4 directions
+    Vector<Directions> v = new Vector<Directions>();
+        for(int i=0;i<4;++i) {
+        v.add(Directions.values()[i]);
+        }
+        Directions directions[]=new Directions[4];
+        for(int i=0;i<directions.length;++i){
+            int index=random.nextInt(v.size());
+            directions[i]=v.get(index);
+            v.remove(index);
+        }
+//pour chacune de ces directions,on avance en profondeur d’abord
+        for(int i=0;i<4;++i){
+            Directions dir=directions[i];
+            if(vertex.inBorders(dir) && graph.doesntExist(vertex,dir)){
+                int x=vertex.getX();
+                int y=vertex.getY();
+                int xt=0,yt=0;
+                switch(dir){
+                    case NORTH:xt=x;yt=y-1;break;
+                    case SOUTH:xt=x;yt=y+1;break;
+                    case EAST:xt=x+1;yt=y;break;
+                    case WEST:xt=x-1;yt=y;break;
+                }
+                Vertex next=new Vertex(xt,yt,vertex.getNbr()+1);
+                graph.addVertex(next);
+                graph.addEdge(vertex,next);
+                buildRandomPath(next);
             }
         }
     }
