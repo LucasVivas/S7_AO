5b37baba11f73d31392528aad6e77d38ee4688fa
Lucas Vivas <lucas.vivas33@gmail.com>
2017-11-23 19:03:00 +0100

    graphtest (2)

--- project/src/org/Model/GraphTest.java
+++ project/src/org/Model/GraphTest.java
@@ -24,17 +24,39 @@ public class GraphTest {
         assertTrue("This graph need to have at least one edge", graphBuild.getNbEdges() > 0);
     }
 
+    @Test
+    public void testAddVertex() throws Exception {
+
+    }
+
+    @Test
     public void testDoesntExist() throws Exception{
         Graph graphExist = Graph.getInstance();
+
         Vertex v1 = new Vertex( 1,1,0);
         Vertex v2 = new Vertex( 1,2,1);
-        graphExist.addVertex(v1);
-        assertNotNull("v1 need to be added to the graph",graphExist);
-        graphExist.addVertex(v2);
-        assertNotNull("v2 need to be added to the graph",graphExist);
+        Vertex v3 = null;
+
         Directions dir1 = Directions.NORTH;
-        assertFalse("vertex (1,0) doesn't exist in this graph", graphExist.doesntExist(v1, dir1));
         Directions dir2 = Directions.SOUTH;
+        Directions dir3 = null;
+
+        graphExist.addVertex(v1);
+        graphExist.addVertex(v2);
+
+        assertFalse("vertex (1,0) doesn't exist in this graph", graphExist.doesntExist(v1, dir1));
         assertTrue("vertex (1,2) exists in this graph", graphExist.doesntExist(v1, dir2));
+
+        try{
+            graphExist.doesntExist(v3,dir1);
+            fail("NullPointerException must be throw");
+        }catch (NullPointerException npe){
+        }
+
+        try{
+            graphExist.doesntExist(v1,dir3);
+            fail("NullPointerException must be throw");
+        }catch (NullPointerException npe){
+        }
     }
 }
