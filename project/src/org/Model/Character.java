c8099559d95790b82b3755636dedc6c8e3a3c7ad
LucasVivas <lucas.vivas33@gmail.com>
2017-11-30 17:27:13 +0100

    now Charcter extends vertex

--- project/src/org/Model/Character.java
+++ project/src/org/Model/Character.java
@@ -2,28 +2,9 @@ package org.Model;
 
 import java.util.Iterator;
 
-public class Character {
-    protected int x, y;
-
-    protected Character(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public void setY(int y) {
-        this.y = y;
+public class Character extends Vertex{
+    public Character(int x, int y) {
+        super(x, y);
     }
 
     public boolean move(Directions direction) {
